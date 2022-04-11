55ae1caa52b6f143ddbdfd323774f4a2c9a8754b
Rik Mentink <rik.mentink@icloud.com>
2022-04-11 14:27:44 +0200

    Created override and backoffice view

--- mx_bestcombination.php
+++ mx_bestcombination.php
@@ -1,28 +1,4 @@
 <?php
-/**
-* 2007-2022 PrestaShop
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Academic Free License (AFL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/afl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author    PrestaShop SA <contact@prestashop.com>
-*  @copyright 2007-2022 PrestaShop SA
-*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
-*  International Registered Trademark & Property of PrestaShop SA
-*/
 
 if (!defined('_PS_VERSION_')) {
     exit;
@@ -59,7 +35,7 @@ class Mx_bestcombination extends Module
 
         return parent::install() &&
             $this->registerHook('header') &&
-            $this->registerHook('backOfficeHeader');
+            $this->registerHook('displayBackOfficeHeader');
     }
 
     public function uninstall()
@@ -147,19 +123,6 @@ class Mx_bestcombination extends Module
                             )
                         ),
                     ),
-                    array(
-                        'col' => 3,
-                        'type' => 'text',
-                        'prefix' => '<i class="icon icon-envelope"></i>',
-                        'desc' => $this->l('Enter a valid email address'),
-                        'name' => 'MX_BESTCOMBINATION_ACCOUNT_EMAIL',
-                        'label' => $this->l('Email'),
-                    ),
-                    array(
-                        'type' => 'password',
-                        'name' => 'MX_BESTCOMBINATION_ACCOUNT_PASSWORD',
-                        'label' => $this->l('Password'),
-                    ),
                 ),
                 'submit' => array(
                     'title' => $this->l('Save'),
@@ -175,8 +138,6 @@ class Mx_bestcombination extends Module
     {
         return array(
             'MX_BESTCOMBINATION_LIVE_MODE' => Configuration::get('MX_BESTCOMBINATION_LIVE_MODE', true),
-            'MX_BESTCOMBINATION_ACCOUNT_EMAIL' => Configuration::get('MX_BESTCOMBINATION_ACCOUNT_EMAIL', 'contact@prestashop.com'),
-            'MX_BESTCOMBINATION_ACCOUNT_PASSWORD' => Configuration::get('MX_BESTCOMBINATION_ACCOUNT_PASSWORD', null),
         );
     }
 
@@ -190,25 +151,5 @@ class Mx_bestcombination extends Module
         foreach (array_keys($form_values) as $key) {
             Configuration::updateValue($key, Tools::getValue($key));
         }
-    }
-
-    /**
-    * Add the CSS & JavaScript files you want to be loaded in the BO.
-    */
-    public function hookBackOfficeHeader()
-    {
-        if (Tools::getValue('module_name') == $this->name) {
-            $this->context->controller->addJS($this->_path.'views/js/back.js');
-            $this->context->controller->addCSS($this->_path.'views/css/back.css');
-        }
-    }
-
-    /**
-     * Add the CSS & JavaScript files you want to be added on the FO.
-     */
-    public function hookHeader()
-    {
-        $this->context->controller->addJS($this->_path.'/views/js/front.js');
-        $this->context->controller->addCSS($this->_path.'/views/css/front.css');
     }
 }
--- views/templates/admin/configure.tpl
+++ views/templates/admin/configure.tpl
@@ -26,23 +26,17 @@
 <div class="panel">
 	<h3><i class="icon icon-credit-card"></i> {l s='Find Best Combination' mod='mx_bestcombination'}</h3>
 	<p>
-		<strong>{l s='Here is my new generic module!' mod='mx_bestcombination'}</strong><br />
-		{l s='Thanks to PrestaShop, now I have a great module.' mod='mx_bestcombination'}<br />
-		{l s='I can configure it using the following configuration form.' mod='mx_bestcombination'}
+		<strong>{l s='About this module' mod='mx_bestcombination'}</strong><br />
+		{l s='Provides customers with the best possible product combination on your PrestaShop e-commerce store.' mod='mx_bestcombination'}
 	</p>
-	<br />
 	<p>
-		{l s='This module will boost your sales!' mod='mx_bestcombination'}
+		{l s='When you have a product with multiple attributes but only a few combinations exist, this module will always make sure that your customers are provided with the best possible combination. This prevents the customer from having to deal with Ajax errors, and allow them to buy your product as quickly as possible.' mod='mx_bestcombination'}
 	</p>
-</div>
-
-<div class="panel">
-	<h3><i class="icon icon-tags"></i> {l s='Documentation' mod='mx_bestcombination'}</h3>
 	<p>
-		&raquo; {l s='You can get a PDF documentation to configure this module' mod='mx_bestcombination'} :
-		<ul>
-			<li><a href="#" target="_blank">{l s='English' mod='mx_bestcombination'}</a></li>
-			<li><a href="#" target="_blank">{l s='French' mod='mx_bestcombination'}</a></li>
-		</ul>
+		{l s='In the card below you can enable or disable the generic function of this module.'}
+	</p>
+	<p>
+		<strong>{l s='Developers' mod='mx_bestcombination'}</strong>
+		{l s='Are you a developer and would you like to contribute to perfecting this module? Feel free to open a suggestion or commit to this module yourself on the'}<a href="https://github.com/rikmentink/mx_bestcombination" title="rikmentink/mx_bestcombination">{l s='GitHub repository%' mod='mx_bestcombination'}</a></strong>
 	</p>
 </div>
